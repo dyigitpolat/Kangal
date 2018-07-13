bed8f443dbc9cfeaba56e9e6dd9ef1866e91072c
yigit polat <yigit.polat@ug.bilkent.edu.tr>
2018-07-13 20:46:02 +0300

    Player class

--- src/data/Player.java
+++ src/data/Player.java
@@ -1,7 +1,25 @@
 package data;
 
-public class Player {
+import java.util.Date;
 
-	
-
+enum Nationality 
+{
+	TURKEY, GERMANY, ENGLAND;
 }
+
+public class Player {
+	private String name;
+	private Nationality nationality;
+	private Date birthday;
+	private Picture picture;
+	
+	public Player(String name, Date bday, Nationality nation)
+	{
+		picture = null;
+		this.name = name;
+		birthday = bday;
+		nationality = nation;
+	}
+}
+
+
