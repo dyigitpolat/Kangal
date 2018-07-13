720c0ffa66b84602a95deeb27985dbf8c8aa7cae
yigit polat <yigit.polat@ug.bilkent.edu.tr>
2018-07-13 18:22:53 +0300

    first commit

---
+++ src/Kangal.java
@@ -0,0 +1,15 @@
+import ui.Screen;
+
+
+public class Kangal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Screen s = new Screen();
+		
+
+	}
+
+}
---
+++ src/ui/Screen.java
@@ -0,0 +1,17 @@
+package ui;
+import javax.swing.*;;
+
+public class Screen {
+	
+	private JFrame screenFrame;
+	
+	
+	public Screen() {
+		System.out.println("Screen initiated.");
+	}
+	
+	public void render() {
+		
+	}
+	
+}
