7864697626fe0f74a46a9f185f8b0f4de570917c
Yigit Polat <yigit.polat@ug.bilkent.edu.tr>
2018-07-27 13:43:40 +0300

    asdsdffg

--- src/KangalFactory.java
+++ src/KangalFactory.java
@@ -1,6 +1,38 @@
+import ui.*;
+import data.*;
 
 public class KangalFactory {
-	public KangalFactory() {
+	
+	public KangalFactory( String app) throws Exception {
+		if( app.equals("basketball")) {
+			constructBasketballApp();
+		} else if ( app.equals("soccer")) {
+			
+			
+		} else if ( app.equals("tennis")) {
+			
+		} else {
+			throw new Exception( "failure.");
+		}
+	}
+	
+	private void constructBasketballApp() {
+		DatabaseManager dbm = new DatabaseManager();
+		Screen screen = new Screen("Basketball");
+		LoginScene login = new BasketballLoginScene(screen);
+		SearchScene search = new SearchScene(dbm, screen);
+		
+		login.setNext(search);
+		screen.setScene(login);
+		
+	}
+	
+	private void constructSoccerApp() {
+
+		
+	}
+	
+	private void constructTennisApp() {
 		
 	}
 }
